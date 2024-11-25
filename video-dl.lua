017fe813e70211cf97b3aefbef360265ecf99daf
Tangent <rose@tangentfox.com>
2024-11-24 18:29:53 -0700

    forgot to make video-dl use utility-functions

--- video-dl.lua
+++ video-dl.lua
@@ -18,9 +18,15 @@ local help = [[Usage:
             yt-dlp works with.
 ]]
 
-if os.execute("where yt-dlp") ~= 0 then
-  error("yt-dlp must be installed and in the path")
+local success, utility = pcall(function()
+  return dofile((arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "utility-functions.lua")
+end)
+if not success then
+  print("\n\n" .. tostring(utility))
+  error("\n\nThis script may be installed improperly. Follow instructions at:\n\thttps://github.com/TangentFoxy/.lua-files#installation\n")
 end
+
+utility.required_program("yt-dlp")
 
 local action, url
 
