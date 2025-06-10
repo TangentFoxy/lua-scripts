6a1db5c09bfec929875d1f0beb4b8e195445fa4e
Rose Liverman <rose@tangentfox.com>
2025-06-10 03:54:45 -0600

    something that addresses #55

---
+++ a-script.lua
@@ -0,0 +1,2 @@
+print("working")
+return true
--- test.lua
+++ test.lua
@@ -1,5 +1,24 @@
 #!/usr/bin/env luajit
 
+-- according to PiL 8.1, setting LUA_PATH should make require work, but it doesn't
+--   https://www.lua.org/pil/8.1.html
+print((arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")))
+for k,v in pairs(arg) do print(k,v) end
+
+for k,v in pairs(package) do print(k,v) end
+
+-- finally, something that works consistently!
+package.path = (arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "?.lua;" .. package.path
+require "a-script"
+
+-- LUA_PATH=(arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "?.lua"
+-- print(LUA_PATH)
+-- require "a-script"
+
+-- dofile((arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "a-script.lua")
+
+if true then return end
+
 local success, utility = pcall(function()
   return dofile((arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "utility-functions.lua")
 end)
