186af8a00484fa5b575938fbf7b60abd1b3d29e7
Rose Liverman <rose@tangentfox.com>
2025-06-11 01:10:36 -0600

    okay, finally, something usable.. badly

--- add_to_path.sh
+++ add_to_path.sh
@@ -1,8 +1,8 @@
-if echo "$PATH" | grep -q -E "(^|:)$current_dir(:|$)"; then
-  echo "lua-scripts is already in your \$PATH."
-else
-  echo "export PATH=\$PATH:$PWD" >> ~/.bashrc
-  sleep 1
-  source ~/.bashrc
-  echo "lua-scripts has been added to your \$PATH."
-fi
+echo "WARNING: This script should only be run once."
+read -p "Press enter to continue, or close this terminal (ctrl+c)." dummy
+
+echo "export PATH=\$PATH:$PWD" >> ~/.bashrc
+echo "lua-scripts has been added to your \$PATH."
+echo ""
+echo "Due to a bug, I can't make THIS terminal update its path."
+echo "Run \"source ~/.bashrc\" next or start a new terminal."
