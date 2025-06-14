1c0b5d7a1aaa7e1b0399d267aa026071621cbfc5
Rose Liverman <rose@tangentfox.com>
2025-06-14 16:33:06 -0600

    new script for pulling down and making individual epubs from html

---
+++ html2epub.lua
@@ -0,0 +1,75 @@
+#!/usr/bin/env luajit
+
+package.path = (arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "lib" .. package.config:sub(1, 1) .. "?.lua;" .. package.path
+local utility = require "utility"
+
+local htmlparser = require "htmlparser"
+local json = require "json"
+
+local options = utility.open(arg[1], "r")(function(file)
+  return json.decode(file:read("*all"))
+end)
+
+local function equal_array_lengths(...)
+  local arg = {...}
+  for i = 2, #arg do
+    if #arg[i] ~= #arg[1] then
+      return false
+    end
+  end
+  return true
+end
+
+os.execute("mkdir -p Stories")
+os.execute("mkdir -p \"All ePubs\"")
+
+utility.open(options.index_file, "r")(function(file)
+  local html_text = file:read("*all")
+  local parser = htmlparser.parse(html_text, 10000)
+
+  local titles = parser:select(options.title_pattern)
+  local links = parser:select(options.link_pattern)
+  local date = parser:select(options.date_pattern)
+  local metadata = parser:select(options.metadata_pattern)
+
+  if not equal_array_lengths(titles, links, date, metadata) then
+    error("Selectors did not get the same number of items across each type.")
+  end
+
+  for index in ipairs(titles) do
+    local page = {
+      title = titles[index]:getcontent(),
+      link = links[index].attributes.href,
+      date = date[index]:getcontent(),
+      metadata = metadata[index]:getcontent(),
+    }
+
+    local output = "<h1>" .. page.title .. "</h1>\n"
+    output = output .. "<p>" .. page.date .. " " .. page.metadata .. "</p>\n"
+
+    local html_text = utility.curl_read(page.link)
+    local parser = htmlparser.parse(html_text, 10000)
+    local content = parser:select(options.content_pattern)
+    output = output .. content[1]:getcontent() .. "\n"
+
+    page.base_file_name = utility.make_safe_file_name(page.title)
+    local html_file_path = ("Stories" .. utility.path_separator .. page.base_file_name .. ".html"):enquote()
+    local epub_file_path = ("All ePubs" .. utility.path_separator .. page.base_file_name .. ".epub"):enquote()
+    utility.open(("Stories" .. utility.path_separator .. page.base_file_name .. ".html"), "w")(function(file)
+      file:write(output)
+    end)
+
+    utility.open("metadata.yaml", "w")(function(file)
+      file:write("---\ntitle: \"" .. page.title .. "\"\nauthor:\n")
+      for _, author in ipairs(options.authors) do
+        file:write("- " .. author:enquote() .. "\n")
+      end
+      file:write("---\n")
+    end)
+    os.execute("pandoc --metadata-file metadata.yaml --from html --to epub " .. html_file_path .. " -o " .. epub_file_path .. " --toc=true")
+
+    os.execute("sleep " .. tostring(math.random(5)))
+  end
+end)
+
+os.execute("rm metadata.yaml")
