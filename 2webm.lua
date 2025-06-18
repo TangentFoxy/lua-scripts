5a9ca8ffdf134097d65a5dc6049394e82c34d9aa
Rose Liverman <rose@tangentfox.com>
2025-06-18 00:07:10 -0600

    2webm uses argparse

--- 2webm.lua
+++ 2webm.lua
@@ -1,37 +1,31 @@
 #!/usr/bin/env luajit
 
-local help = [[Usage:
-
-  2webm.lua [threads=1]
-
-Converts everything in the local directory to webm, placed in "./2webm-output".
-(Defaults to using only a single thread to reduce impact on the system.)
-
-[threads]: Number of threads ffmpeg will be assigned.
-           If a non-number value, ffmpeg's -threads flag will not be used.
-]]
-
-if arg[1] and arg[1]:find("help") then
-  print(help)
-  return false
-end
-
 package.path = (arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "lib" .. package.config:sub(1, 1) .. "?.lua;" .. package.path
 local utility = require "utility"
+local argparse = utility.require("argparse")
+
+local parser = argparse():description("Converts everything in the local directory to webm, placed in \"./2webm-output\".")
+parser:argument("threads", "Number of threads ffmpeg will be assigned."):convert(tonumber):args("?")
+local options = parser:parse()
+
+
 
 utility.required_program("ffmpeg")
-
-local threads = tonumber(arg[1]) or arg[1] or 1
 
 local for_files = utility.ls()
 os.execute("mkdir 2webm-output")
 
 for_files(function(file_name)
+  local _, name, extension = utility.split_path_components(file_name)
+  if extension then
+    name = name:sub(1, -(#extension + 2))
+  end
+
   local command
-  if type(threads) == "number" then
-    command = "ffmpeg -threads " .. threads .. " -i \"" .. file_name .. "\" -threads " .. threads .. " \"2webm-output/" .. file_name .. ".webm\""
+  if options.threads then
+    command = "ffmpeg -threads " .. options.threads .. " -i \"" .. file_name .. "\" -threads " .. options.threads .. " \"2webm-output/" .. name .. ".webm\""
   else
-    command = "ffmpeg -i \"" .. file_name .. "\" \"2webm-output/" .. file_name .. ".webm\""
+    command = "ffmpeg -i \"" .. file_name .. "\" \"2webm-output/" .. name .. ".webm\""
   end
 
   os.execute(command)
