a56db4a9b9651a873d68c0b91df2c04122e796a6
Rose Liverman <rose@tangentfox.com>
2025-06-18 05:00:01 -0600

    gopro sort using util func

--- gopro-video-sort.lua
+++ gopro-video-sort.lua
@@ -1,31 +1,17 @@
 #!/usr/bin/env luajit
 
--- OS must be detected to choose list and move commands
-local ls, move
-if package.config:sub(1,1) == "\\" then
-  ls = "dir /w /b > files.txt"
-  move = "move"
-else
-  ls = "ls -1 > files.txt"
-  move = "mv"
-end
+package.path = (arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "lib" .. package.config:sub(1, 1) .. "?.lua;" .. package.path
+local utility = require "utility"
 
-os.execute(ls)
-
-local file = io.open("files.txt")
 local created = {}
 
--- put in table of tables (folder organization)
--- go through whole structure making folders and moving files
-for line in file:lines() do
-  if line:find("%.MP4") then
-    local folder = tonumber(line:sub(9-3, 9-1)) -- sequence number
+utility.ls(".")(function(file_name)
+  if file_name:find("%.MP4") then
+    local folder = tonumber(file_name:sub(9-3, 9-1)) -- sequence number
     if not created[folder] then
       created[folder] = true
-      os.execute("mkdir "..folder)
+      os.execute("mkdir " .. folder)
     end
-    os.execute(move .. " "..line.." "..folder)
+    os.execute(utility.commands.move .. file_name .. " " .. folder)
   end
-end
-
-os.remove("files.txt")
+end)
