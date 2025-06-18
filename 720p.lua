66727599144ffed707d0dcc19e0e7a2c1800fa04
Rose Liverman <rose@tangentfox.com>
2025-06-18 00:22:43 -0600

    720p.lua updated to argparse.. despite its very bad flaws

--- 720p.lua
+++ 720p.lua
@@ -1,57 +1,39 @@
 #!/usr/bin/env luajit
 
-local help = [[Usage:
-
-  720p.lua [tune=film] [threads=1]
-
-Converts everything in the local directory to 720p MP4s, placed in
-"./720p-output". (Defaults to using only a single thread to reduce impact on
-the system.)
-
-[tune]:    Improve encoding by specifying type of video. "film" is the default.
-           Can be "grain" for grainy sources, "animation", or "stillimage".
-[threads]: Number of threads ffmpeg will be assigned.
-           If a non-number value, ffmpeg's -threads flag will not be used.
-]]
-
-if arg[1] and arg[1]:find("help") then
-  print(help)
-  return false
-end
-
 package.path = (arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "lib" .. package.config:sub(1, 1) .. "?.lua;" .. package.path
 local utility = require "utility"
+local argparse = utility.require("argparse")
+
+local parser = argparse():description("Converts everything in the local directory to 720p mp4s, placed in \"./720p-output\".")
+-- BUG argparse won't assign a default tune; and won't accept multiple optional arguments even though their required forms makes it obvious which is which
+-- parser:argument("tune", "Improve encoding by specifying type of video. \"grain\" is for grainy film. Others are self-explanatory.")
+--   :choices{"film", "grain", "animation", "stillimage"}:default("film"):defmode("arg"):args("?")
+parser:argument("threads", "Number of threads ffmpeg will be assigned."):convert(tonumber):args("?")
+parser:option("--tune", "Improve encoding by specifying type of video. \"grain\" is for grainy film. Others are self-explanatory.")
+  :choices{"film", "grain", "animation", "stillimage"}:default("film"):defmode("arg"):args("?")
+local options = parser:parse()
+
+options.tune = options.tune[1] or "film"
+print(options.tune)
+
+
 
 utility.required_program("ffmpeg")
-
-local tune
-if arg[2] then
-  tune = arg[1]
-  arg[1] = arg[2]
-end
-local threads = tonumber(arg[1]) or arg[1] or 1
-
-local tunes = { "film", "grain", "animation", "stillimage", }
-local valid_tune = false
-for _, tune_option in ipairs(tunes) do
-  if tune == tune_option then
-    valid_tune = true
-    break
-  end
-end
-if not valid_tune then
-  tune = "film"
-end
 
 local for_files = utility.ls()
 os.execute("mkdir 720p-output")
 
 for_files(function(file_name)
+  local _, name, extension = utility.split_path_components(file_name)
+  if extension then
+    name = name:sub(1, -(#extension + 2))
+  end
+
   local command
-  if type(threads) == "number" then
-    command = "ffmpeg -threads " .. threads .. " -i \"" .. file_name .. "\" -vf scale=1280:-2 -threads " .. threads .. " -tune " .. tune .. " -crf 28 \"720p-output/" .. file_name .. ".mp4\""
+  if options.threads then
+    command = "ffmpeg -threads " .. options.threads .. " -i \"" .. file_name .. "\" -vf scale=1280:-2 -threads " .. options.threads .. " -tune " .. options.tune .. " -crf 28 \"720p-output/" .. name .. ".mp4\""
   else
-    command = "ffmpeg -i \"" .. file_name .. "\" -vf scale=1280:-2 -tune " .. tune .. " -crf 28 \"720p-output/" .. file_name .. ".mp4\""
+    command = "ffmpeg -i \"" .. file_name .. "\" -vf scale=1280:-2 -tune " .. options.tune .. " -crf 28 \"720p-output/" .. name .. ".mp4\""
   end
 
   os.execute(command)
