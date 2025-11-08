fdc5933a77663836db98dfc7eaab16f6fbaf50c1
Tangent <rose@tangentfox.com>
2025-11-07 21:11:59 -0700

    updated ksp.lua a whole lot

--- ksp.lua
+++ ksp.lua
@@ -1,61 +1,152 @@
 #!/usr/bin/env luajit
 
--- NOTE it is assumed all mods use ModuleManager; it remains in place in GameData
-
 package.path = (arg[0]:match("@?(.*/)") or arg[0]:match("@?(.*\\)")) .. "lib" .. package.config:sub(1, 1) .. "?.lua;" .. package.path
 local utility = require "utility"
 local json = utility.require("dkjson")
 
-local file = io.open("Mods/mods.json")
-local text = file:read("*all")
-local data = json.decode(text)
+local function os_move(source, destination)
+  os.execute(utility.commands.move .. source:enquote() .. " " .. destination:enquote())
+end
 
-local mods = data.mods
-local lists = data.lists
-
-
-
-local selected_list = table.concat(arg, " ")
-assert(#selected_list > 0, "Call ksp.lua with a named mod list.")
-
-local list = lists[selected_list]
-assert(list, "'" .. selected_list .. "' is not a defined list.")
-
-
-
--- convert list selection into a hashtable of names for all needed files
---  (this handles duplicated names from dependencies)
-local file_names_hashtable = {}
-for _, mod_name in ipairs(list) do
-  if type(mod_name) == "string" then
-    local mod_list = mods[mod_name]
-    if mod_list then
-      for _, mod_name in ipairs(mod_list) do
-        if type(mod_name) == "string" then
-          file_names_hashtable[mod_name] = true
-        end
+local function find_orphan_mods(mods)
+  local mod_files_hashtable = {}
+  for name, mod in pairs(mods) do
+    if mod == true then
+      mod_files_hashtable[name] = true
+    elseif mod.list and (not mod.missing) then
+      for _, name in ipairs(mod.list) do
+        mod_files_hashtable[name] = true
       end
+      -- mod.dependencies uses the name you provide rather than the directory name, so it is out of place here
+      -- if mod.dependencies then
+      --   for _, name in ipairs(mod.dependencies) do
+      --     mod_files_hashtable[name] = true
+      --   end
+      -- end
     else
-      file_names_hashtable[mod_name] = true
+      for _, name in ipairs(mod) do
+        mod_files_hashtable[name] = true
+      end
+    end
+  end
+  local found_files_hashtable = {}
+  local ignored_files = utility.enumerate{".", "..", ".DS_Store"}
+  utility.ls("Mods", function(file_name)
+    if ignored_files[file_name] then return end
+    found_files_hashtable[file_name] = true
+    if not mod_files_hashtable[file_name] then
+      print(file_name:enquote() .. " is present, but unlisted.")
+    end
+  end)
+  for name in pairs(mod_files_hashtable) do
+    if not found_files_hashtable[name] then
+      print(name:enquote() .. " is listed, but missing.")
     end
   end
 end
 
-local file_names_list = {}
-for mod_name in pairs(file_names_hashtable) do
-  table.insert(file_names_list, mod_name)
+-- ModuleManager should always be present in GameData
+local function module_manager_check()
+  local found = false
+  utility.ls("GameData", function(file_name)
+    if file_name:find("ModuleManager") then
+      found = true
+    end
+  end)
+  assert(found, "ModuleManager must be present in GameData.")
+end
+module_manager_check()
+
+local function load_instance_file()
+  local file = io.open("instance.json")
+  assert(file, "instance.json must in KSP's root directory.")
+  local text = file:read("*all")
+  local data = json.decode(text)
+  return data.mods, data.instances
+end
+local mods, instances = load_instance_file()
+
+
+
+local selected_instance = table.concat(arg, " ")
+assert(#selected_instance > 0, "Call ksp.lua with an instance's name.")
+
+if selected_instance == "--find-orphans" then
+  find_orphan_mods(mods)
+  os.exit()
 end
 
-local function move_list(list, a, b)
-  local function os_move(a, b)
-    os.execute("mv \"" .. a .. "\" \"" .. b .. "\"")
+local instance = instances[selected_instance]
+assert(instance, selected_instance:enquote() .. " is not a named instance.")
+
+
+
+-- select dependencies
+local dependencies = {}
+for _, name in ipairs(instance.mods or {}) do
+  local list = type(mods[name]) == "table" and mods[name].dependencies
+  if list then
+    for _, name in ipairs(list) do
+      dependencies[name] = true
+    end
+  end
+end
+for name in pairs(dependencies) do
+  instance.mods[#instance.mods + 1] = name
+end
+
+-- prepare mod move commands
+local start_move_queue, cleanup_move_queue = {}, {}
+for _, name in ipairs(instance.mods or {}) do
+  local mod = mods[name]
+  assert(mod, "Mod " .. name:enquote() .. " is not defined.")
+  if mod == true then
+    start_move_queue["Mods/" .. name] = "GameData"
+    cleanup_move_queue["GameData/" .. name] = "Mods"
+  elseif mod.subdirectory then
+    os.execute("mkdir -p " .. mod.subdirectory:enquote()) -- WARNING may not work multiplatform
+    for _, name in ipairs(mod.list) do
+      start_move_queue["Mods/" .. name] = mod.subdirectory
+      cleanup_move_queue[subdirectory .. utility.path_separator .. name] = "Mods"
+    end
+  elseif mod.list then
+    for _, name in ipairs(mod.list) do
+      start_move_queue["Mods/" .. name] = "GameData"
+      cleanup_move_queue["GameData/" .. name] = "Mods"
+    end
+  else
+    for _, name in ipairs(mod) do
+      start_move_queue["Mods/" .. name] = "GameData"
+      cleanup_move_queue["GameData/" .. name] = "Mods"
+    end
   end
 
-  for _, mod_name in ipairs(list) do
-    os_move(a .. "/" .. mod_name, b .. "/")
+  if type(mod) == "table" then
+    if mod.incomplete then
+      print("Mod " .. name:enquote() .. " is marked as incomplete and will not work correctly.")
+    end
+    if mod.missing then
+      print("Mod " .. name:enquote() .. " is marked as missing.")
+    end
+    if mod.broken then
+      print("Mod " .. name:enquote() .. " is marked as broken and may not work correctly.")
+    end
   end
 end
 
-move_list(file_names_list, "Mods", "GameData")
-os.execute("./KSP.app/Contents/MacOS/KSP")
-move_list(file_names_list, "GameData", "Mods")
+for _, name in ipairs(instance.saves or {}) do
+  start_move_queue["InstanceSaves/" .. name] = "saves"
+  cleanup_move_queue["saves/" .. name] = "InstanceSaves"
+end
+
+
+
+for source, destination in pairs(start_move_queue) do
+  os_move(source, destination)
+end
+
+os.execute("./KSP.app/Contents/MacOS/KSP") -- TODO fix for multiplatform
+
+for source, destination in pairs(cleanup_move_queue) do
+  os_move(source, destination)
+end
