ef6c42b6b657dd8bc3f3b0c580435e30612d8110
Rose Liverman <rose@tangentfox.com>
2026-01-07 05:49:18 -0600

    close #77 fixed .bashrc include

--- add_to_path.sh
+++ add_to_path.sh
@@ -1,6 +1,7 @@
 echo "WARNING: This script should only be run once."
 read -p "Press enter to continue, or close this terminal (ctrl+c)." dummy
 
+echo >> ~/.bashrc
 echo "export PATH=\$PATH:$PWD" >> ~/.bashrc
 echo "lua-scripts has been added to your \$PATH."
 echo ""
